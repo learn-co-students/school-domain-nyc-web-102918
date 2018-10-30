adac043a5a4417fdd7e37aed3896c611763ffbaa
jbmenashi <jbmenashi@gmail.com>
2018-10-30 17:27:31 -0400

    passed all tests

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, student_array|
+      @roster[grade] = student_array.sort
+    end
+    @roster
+  end
+
+
+
+end
