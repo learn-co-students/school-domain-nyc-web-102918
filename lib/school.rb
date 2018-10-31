65440d493514f2de4674730765c06f2a57323812
hsterman <heather.sterman531@gmail.com>
2018-10-30 22:29:32 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
 # code here!
+class School
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.grade(9)
+school.sort
+school.roster = {}
+end
